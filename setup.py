e863be7bef331b6ed49430a079ac04b386281b30
Lyuboslav Petrov <petrov.lyuboslav@gmail.com>
2024-12-07 12:58:00 +0200

    Bump version to satisfy pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="openapi-cli-generator",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "pyyaml>=6.0.2",
